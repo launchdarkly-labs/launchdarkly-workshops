3d1789f8b5ddaf4644db79017e38269b16186189
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2024-04-16 18:25:20 -0400

    add rust

---
+++ povs/rust/pov-rust-image.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update
+
+# Install tools
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /etc/apt/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+apt-get -y update
+apt-get -y autoremove
+apt-get -y install unzip jq git curl gnupg ca-certificates terraform vim
+
+# Cleanup and install NodeJS
+apt-get install -y nodejs
+npm install -g npm@latest
+
+# Install Rust
+######################
+
+apt-get install -y rustc cargo
+cargo install cargo-watch
+echo "export PATH=\$PATH:/root/.cargo/bin" >> ~/.profile
+echo "export PATH=\$PATH:/root/.cargo/bin" >> ~/.bashrc
+
+mkdir -p /opt/rust
+cd /opt/rust
+git clone https://github.com/launchdarkly-labs/ld-sample-app-rust.git
+cd ld-sample-app-rust
+
+######################
+
+# clone the Terraform code to generate user
+mkdir -p /opt/ld
+cd /opt/ld
+git clone https://github.com/kevincloud/terraform-ld-student.git
+cd /opt/ld/terraform-ld-student
+terraform init
+
+# Install code editor
+mkdir -p /root/.local/share/code-server/User
+
+cat > /root/.local/share/code-server/User/settings.json <<-EOF
+{
+    "workbench.colorTheme": "Default Dark+",
+    "workbench.startupEditor": "none",
+    "security.workspace.trust.enabled": false,
+}
+EOF
+
+curl -fsSL https://code-server.dev/install.sh | sh
+
+# Create Code Server startup script
+cat <<-EOF > /etc/systemd/system/code-server.service
+[Unit]
+Description=Code Server
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/usr/bin/code-server --host 0.0.0.0 --port 8080 --auth none /opt/rust/
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start Code Server
+systemctl enable code-server
+systemctl start code-server
+
+# code-server --install-extension ms-python.python --user-data-dir /user-data
+
+mkdir /opt/ld/flag
+cat <<-EOF > /opt/ld/flag/main.tf
+terraform {
+  required_providers {
+    launchdarkly = {
+      source  = "launchdarkly/launchdarkly"
+      version = "~>2.15"
+    }
+  }
+}
+
+variable "project_key" {
+}
+
+provider "launchdarkly" {
+}
+
+resource "launchdarkly_feature_flag" "release_storefront_flag" {
+  project_key = var.project_key
+  key         = "test-flag"
+  name        = "Test Flag"
+
+  variation_type = "boolean"
+
+  variations {
+    value = "true"
+    name  = "True"
+  }
+  variations {
+    value = "false"
+    name  = "False"
+  }
+
+  client_side_availability {
+    using_environment_id = true
+    using_mobile_key     = false
+  }
+
+  defaults {
+    on_variation  = 0
+    off_variation = 1
+  }
+
+  tags = [
+    "managed-by-terraform"
+  ]
+}
+EOF
+
+cd /opt/ld/flag
+terraform init
---
+++ povs/rust/pov-sandbox-rust/01-using-the-launchdarkly-rust-sdk/assignment.md
@@ -0,0 +1,36 @@
+---
+slug: using-the-launchdarkly-rust-sdk
+id: wib9yxlmwybs
+type: challenge
+title: Using the LaunchDarkly Rust SDK
+teaser: Getting started with the LaunchDarkly SDK is easy. This track provides you
+  with a Rust environment which is ready to go!
+notes:
+- type: text
+  contents: Are you ready to being your PoV? This environment provides you with everything
+    you need to see exactly how the LaunchDarkly Rust SDK can work for you.
+tabs:
+- title: Code Editor
+  type: service
+  hostname: workstation
+  port: 8080
+- title: LaunchDarkly
+  type: browser
+  hostname: launchdarkly
+- title: Shell
+  type: terminal
+  hostname: workstation
+difficulty: basic
+timelimit: 600
+---
+# Success Criteria
+
+Sample application is located in:
+`/opt/rust/ld-sample-app-rust`
+
+Your success criteria for this PoV is:
+* Use case 1
+* Use case 2
+* Use case 3
+* Use case 4
+* Use case 5
